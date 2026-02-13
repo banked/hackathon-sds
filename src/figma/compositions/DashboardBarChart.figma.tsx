8e82f0a800776c19593b5aebdae59f41dffef931
George Wiggin <george@sequest.io>
2026-02-13 17:36:52 +0000

    Figma sync: design update (node 6:513)

--- src/figma/compositions/DashboardBarChart.figma.tsx
+++ src/figma/compositions/DashboardBarChart.figma.tsx
@@ -1,8 +1,24 @@
 import figma from "@figma/code-connect";
 import {
+  DashboardBarChart,
   GraphHeader,
   GraphVisualisation,
 } from "compositions";
+
+figma.connect(
+  DashboardBarChart,
+  "https://www.figma.com/design/RunXbtjgAaAIsNgvyEndOD/MCP-Design-System?node-id=6-513&m=dev",
+  {
+    example: () => (
+      <DashboardBarChart
+        title="Title text"
+        aiButtons={[{ label: "Analyse" }]}
+        keys={[{ label: "Label" }, { label: "Label" }, { label: "Label" }]}
+        dates={[{ label: "01 Aug" }, { label: "01 Aug" }]}
+      />
+    ),
+  },
+);
 
 figma.connect(
   GraphHeader,
